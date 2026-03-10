206b723337d046550568d895456939e27f29ec12
topgrfss <topg.rfss@tutamail.com>
2026-03-10 20:33:42 +0300

    doing stuf

--- src/storage.cpp
+++ src/storage.cpp
@@ -1,20 +1,24 @@
 #include "storage.hpp"
 
-#include <algorithm>
 #include <cstring>
 #include <optional>
 
-// todo
-//
-//
-//
-
 void Storage::put(const Item *item)
 {
+	Item *item_to_find = find_item((*item).key);
+
+	if (item_to_find != nullptr) {
+		std::strncpy(item_to_find->value, (*item).value,
+			     sizeof(item_to_find->value) - 1);
+		item_to_find->value[sizeof(item_to_find->value) - 1] = '\0';
+		return;
+	}
+	items_.push_back(*item_to_find);
 }
 
 std::optional<Item> Storage::get(const char *key)
 {
+	return *find_item(key);
 }
 
 void Storage::remove(const char *key)
@@ -28,4 +32,10 @@ int Storage::count() const
 
 Item *Storage::find_item(const char *key)
 {
+	for (Item &item : items_) {
+		if (std::strcmp(item.key, key) == 0) {
+			return &item;
+		}
+	}
+	return nullptr;
 }
